7a47f800602c5161460c1a5d5f72a3e46462d196
Paul7lev <78643463+Paul7lev@users.noreply.github.com>
2021-02-06 16:02:51 +0500

    Add files via upload

---
+++ 22massiv/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+double arr_double2[100];
+
+
+int main() {
+    setlocale(0, "");
+    double arr_double1[100];
+    for (int i = 0; i < 10; i++) {
+        arr_double1[i] = rand();
+    }
+    for (int i = 0; i < 10; i++) {
+        arr_double2[i] = rand();
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << " " << arr_double1[i]
+            << " " << arr_double2[i];
+    }
+}
